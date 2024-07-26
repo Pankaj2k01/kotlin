5aaa5669b57ffe835f028cdb2857930615149506
Pankaj2k01 <109795454+Pankaj2k01@users.noreply.github.com>
2024-07-26 11:17:08 +0530

    Day31 assignments

---
+++ day3Assignment1.kt
@@ -0,0 +1,154 @@
+//Day 3: Interfaces, Encapsulation, and Advanced Concepts / Collections and Generics
+//Task 9: Define an Exportable interface with a method to export transaction data to CSV.
+
+/*interface Exportable {
+    fun exportToCSV(): String
+}
+
+// Update Transaction class to implement Exportable interface
+data class Transaction(val amount: Double, val date: String, val category: String) : Exportable {
+    override fun exportToCSV(): String {
+        return "$amount,$date,$category"
+    }
+}
+
+fun main() {
+    val transaction = Transaction(10.5, "2022-01-01", "Food")
+    println(transaction.exportToCSV()) // Output: 10.5,2022-01-01,Food
+}
+
+//Task 10: Apply encapsulation to Transaction properties using getters and setters ensuring sensitive data is protected.
+
+class Transaction(private var _amount: Double, private var _date: String, private var _category: String) {
+    val amount: Double
+        get() = _amount
+
+    val date: String
+        get() = _date
+
+    val category: String
+        get() = _category
+
+    fun updateAmount(newAmount: Double) {
+        if (newAmount > 0) {
+            _amount = newAmount
+        }
+    }
+
+    fun updateDate(newDate: String) {
+        _date = newDate
+    }
+
+    fun updateCategory(newCategory: String) {
+        _category = newCategory
+    }
+}
+
+fun main() {
+    val transaction = Transaction(10.5, "2022-01-01", "Food")
+    println(transaction.amount) // Output: 10.5
+
+    transaction.updateAmount(20.0)
+    println(transaction.amount) // Output: 20.0
+}
+
+//Task 11: Create generic functions to handle different types of collections (List, Set, Map) of transactions.
+
+data class Transaction(val amount: Double, val date: String, val category: String)
+
+fun printTransactions(transactions: Iterable<Transaction>) {
+    for (transaction in transactions) {
+        println("Amount: ${transaction.amount}, Date: ${transaction.date}, Category: ${transaction.category}")
+    }
+}
+
+fun main() {
+    val transactionList = listOf(Transaction(10.5, "2022-01-01", "Food"), Transaction(20.0, "2022-01-02", "Utilities"))
+    val transactionSet = setOf(Transaction(10.5, "2022-01-01", "Food"), Transaction(20.0, "2022-01-02", "Utilities"))
+    val transactionMap = mapOf("1" to Transaction(10.5, "2022-01-01", "Food"), "2" to Transaction(20.0, "2022-01-02", "Utilities"))
+
+    printTransactions(transactionList)
+    printTransactions(transactionSet)
+    printTransactions(transactionMap.values)
+}*/
+
+//Task 12: Utilize Kotlin's collection libraries to manage a collection of User objects, enabling the addition and removal of users.
+
+class User(val username: String, val password: String)
+
+class UserManager {
+    private val users: MutableMap<String, User> = mutableMapOf()
+
+    fun addUser(user: User) {
+        users[user.username] = user
+    }
+
+    fun removeUser(username: String) {
+        users.remove(username)
+    }
+
+    fun getUser(username: String): User? {
+        return users[username]
+    }
+
+    fun getAllUsers(): List<User> {
+        return users.values.toList()
+    }
+}
+
+fun main() {
+    val userManager = UserManager()
+    val scanner = Scanner(System.`in`)
+
+    while (true) {
+        println("1. Add user")
+        println("2. Remove user")
+        println("3. Get user")
+        println("4. Get all users")
+        println("5. Exit")
+        print("Choose an option: ")
+
+        when (scanner.nextInt()) {
+            1 -> {
+                print("Enter username: ")
+                val username = scanner.next()
+                print("Enter password: ")
+                val password = scanner.next()
+                userManager.addUser(User(username, password))
+                println("User added successfully!")
+            }
+            2 -> {
+                print("Enter username to remove: ")
+                val username = scanner.next()
+                userManager.removeUser(username)
+                println("User removed successfully!")
+            }
+            3 -> {
+                print("Enter username to get: ")
+                val username = scanner.next()
+                val user = userManager.getUser(username)
+                if (user != null) {
+                    println("User found: ${user.username} - ${user.password}")
+                } else {
+                    println("User not found")
+                }
+            }
+            4 -> {
+                val allUsers = userManager.getAllUsers()
+                if (allUsers.isNotEmpty()) {
+                    println("All users:")
+                    allUsers.forEach { println("${it.username} - ${it.password}") }
+                } else {
+                    println("No users found")
+                }
+            }
+            5 -> {
+                println("Exiting...")
+                return
+            }
+            else -> {
+                println("Invalid option. Please choose a valid option.")
+            }
+        }
+    }
+}
---
+++ day3Assignment2.kt
@@ -0,0 +1,169 @@
+// Task 9: Develop a Schedule class that uses interfaces to ensure that all event types can be scheduled and rescheduled.
+/*interface Schedulable {
+    fun schedule(date: String)
+    fun reschedule(date: String)
+}
+
+class Schedule {
+    private val events: MutableList<Schedulable> = mutableListOf()
+
+    fun addEvent(event: Schedulable) {
+        events.add(event)
+    }
+
+    fun scheduleEvent(event: Schedulable, date: String) {
+        event.schedule(date)
+    }
+
+    fun rescheduleEvent(event: Schedulable, date: String) {
+        event.reschedule(date)
+    }
+}
+
+class Event(val name: String) : Schedulable {
+    private var date: String = ""
+
+    override fun schedule(date: String) {
+        this.date = date
+        println("Event $name scheduled for $date")
+    }
+
+    override fun reschedule(date: String) {
+        this.date = date
+        println("Event $name rescheduled for $date")
+    }
+}
+
+fun main() {
+    val schedule = Schedule()
+    print("Enter event name: ")
+    val eventName = readLine()!!
+    val event = Event(eventName)
+
+    schedule.addEvent(event)
+
+    print("Enter initial date: ")
+    val initialDate = readLine()!!
+    schedule.scheduleEvent(event, initialDate)
+
+    print("Enter new date: ")
+    val newDate = readLine()!!
+    schedule.rescheduleEvent(event, newDate)
+}
+
+// Task 10: Secure the event data with proper encapsulation and visibility modifiers.
+class SecureEvent(private val name: String, private val date: String, private val attendeeCount: Int) {
+    fun getName(): String {
+        return name
+    }
+
+    fun getDate(): String {
+        return date
+    }
+
+    fun getAttendeeCount(): Int {
+        return attendeeCount
+    }
+}
+
+fun main() {
+    print("Enter event name: ")
+    val eventName = readLine()!!
+    print("Enter event date: ")
+    val eventDate = readLine()!!
+    print("Enter attendee count: ")
+    val attendeeCount = readLine()!!.toInt()
+
+    val secureEvent = SecureEvent(eventName, eventDate, attendeeCount)
+    println("Event: ${secureEvent.getName()} on ${secureEvent.getDate()} with ${secureEvent.getAttendeeCount()} attendees.")
+}
+
+// Task 11: Manage a collection of events allowing filtering by date or type using Kotlin's powerful collection operations.
+data class Event(val name: String, val date: String, val attendeeCount: Int)
+
+class EventManager {
+    private val events: MutableList<Event> = mutableListOf()
+
+    fun addEvent(event: Event) {
+        events.add(event)
+    }
+
+    fun getEventsByDate(date: String): List<Event> {
+        return events.filter { it.date == date }
+    }
+
+    fun getEventsByType(type: String): List<Event> {
+        return events.filter { it.name.startsWith(type) }
+    }
+}
+
+fun main() {
+    val eventManager = EventManager()
+
+    while (true) {
+        print("Enter event name (or 'quit' to stop): ")
+        val eventName = readLine()!!
+        if (eventName.lowercase() == "quit") break
+        print("Enter event date: ")
+        val eventDate = readLine()!!
+        print("Enter attendee count: ")
+        val attendeeCount = readLine()!!.toInt()
+
+        val event = Event(eventName, eventDate, attendeeCount)
+        eventManager.addEvent(event)
+    }
+
+    print("Enter date to filter by: ")
+    val date = readLine()!!
+    println("Events on $date: ${eventManager.getEventsByDate(date)}")
+
+    print("Enter type to filter by: ")
+    val type = readLine()!!
+    println("Events of type $type: ${eventManager.getEventsByType(type)}")
+}*/
+
+// Task 12: Use generics to create a flexible DataManager class capable of handling different data types, including attendees and events.
+data class Event(val name: String, val date: String, val attendeeCount: Int)
+data class Attendee(val name: String, val email: String)
+
+class DataManager<T> {
+    private val data: MutableList<T> = mutableListOf()
+
+    fun addData(item: T) {
+        data.add(item)
+    }
+
+    fun getData(): List<T> {
+        return data
+    }
+}
+
+fun main() {
+    val eventDataManager = DataManager<Event>()
+    val attendeeDataManager = DataManager<Attendee>()
+
+    while (true) {
+        print("Enter event name (or 'quit' to stop): ")
+        val eventName = readLine()!!
+        if (eventName.lowercase() == "quit") break
+        print("Enter event date: ")
+        val eventDate = readLine()!!
+        print("Enter attendee count: ")
+        val attendeeCount = readLine()!!.toInt()
+
+        val event = Event(eventName, eventDate, attendeeCount)
+        eventDataManager.addData(event)
+    }
+
+    while (true) {
+        print("Enter attendee name (or 'quit' to stop): ")
+        val attendeeName = readLine()!!
+        if (attendeeName.lowercase() == "quit") break
+        print("Enter attendee email: ")
+        val attendee = Attendee(attendeeName, attendeeEmail)
+        attendeeDataManager.addData(attendee)
+    }
+
+    println("Events: ${eventDataManager.getData()}")
+    println("Attendees: ${attendeeDataManager.getData()}")
+}
